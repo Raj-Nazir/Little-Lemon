e6888cd8e00e899f72b762b8d1b81d8b4e10ff1f
Raj-Nazir <raajnazir59181@gmail.com>
2024-03-18 12:59:13 +0530

    Little Lemon

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+import NavCom from "./Components/NavCom";
+import MainCom from "./Components/MainCom";
+import MenuCom from "./Components/MenuCom";
+import FooterCom from "./Components/FooterCom";
+
+function App() {
+  return (
+    <>
+      <NavCom />
+      <MainCom />
+      <MenuCom />
+      <FooterCom />
+    </>
+  );
+}
+
+export default App;
